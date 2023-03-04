54c14d8390c058545e5f79daae6f8932bea0a19a
pkellner <peter@peterkellner.net>
2023-03-04 12:47:33 -0800

    working

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import Keyv from "keyv";
 import { KeyvAdapter } from "@apollo/utils.keyvadapter";
+import responseCachePlugin from "@apollo/server-plugin-response-cache";
+import { InMemoryLRUCache, KeyValueCache } from "@apollo/utils.keyvaluecache";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -31,6 +33,7 @@ const typeDefs = `#graphql
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    hello: String @cacheControl(maxAge: 240)
   }
 `;
 
@@ -45,10 +48,35 @@ const books = [
   },
 ];
 
+class LoggingCache implements KeyValueCache {
+  constructor(private cache = new InMemoryLRUCache()) {}
+
+  async get(key: string) {
+    const result = await this.cache.get(key);
+    console.log("get", result);
+    return result;
+  }
+
+  async set(key: string, value: string) {
+    const result = await this.cache.set(key, value);
+    console.log("set", result);
+    return result;
+  }
+
+  async delete(key: string) {
+    const result = await this.cache.delete(key);
+    console.log("delete", result);
+    return result;
+  }
+}
+
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
+    hello() {
+      return "hello world!";
+    },
     books: (parent, args, contextValue, info) => {
       info.cacheControl.setCacheHint({ maxAge: 60, scope: "PRIVATE" });
       return books;
@@ -62,7 +90,9 @@ const keyV = new Keyv(`redis://localhost:6379`);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  //cache: new LoggingCache(),
   cache: new KeyvAdapter(keyV),
+  plugins: [responseCachePlugin()],
 });
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
@@ -71,7 +101,33 @@ const server = new ApolloServer({
 //  3. prepares your app to handle incoming requests
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: 4010 },
 });
 
 console.log(`🚀 Server listening at: ${url}`);
+
+// https://github.com/mrdulin/apollo-graphql-tutorial/blob/master/src/stackoverflow/60977550/MyCache.ts
+// another example of cache implementation, more simple
+/*
+import { KeyValueCache, KeyValueCacheSetOptions } from 'apollo-server-caching';
+
+export default class MyCache implements KeyValueCache {
+  private cachedInfo = {};
+
+  public async set(key: string, value: string, options?: KeyValueCacheSetOptions): Promise<void> {
+    this.cachedInfo[key] = value;
+  }
+
+  public async get(key: string): Promise<string | undefined> {
+    if (this.cachedInfo[key]) {
+      return this.cachedInfo[key];
+    }
+  }
+
+  public async delete(key: string): Promise<boolean> {
+    this.cachedInfo[key] = null;
+    return this.cachedInfo[key] === null;
+  }
+}
+
+ */
